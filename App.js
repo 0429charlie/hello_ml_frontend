5ef43b61187b480c71dbd988779f78efe532067b
0429charlie <0429charlie@gmail.com>
2020-05-13 20:51:31 -0700

    Initial commit

---
+++ App.js
@@ -0,0 +1,231 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, {Component} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+import ImagePicker from 'react-native-image-picker';
+
+import axios from 'axios';
+import FormData from 'form-data';
+
+class App extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      file: '',
+      label: 'wefjwelkfj',
+    };
+    this.chooseFile = this.chooseFile.bind(this);
+    this.snedRequest = this.snedRequest.bind(this);
+  }
+
+  chooseFile() {
+    let options = {
+      title: 'Select Image',
+      customButtons: [
+        {name: 'customOptionKey', title: 'Choose Photo from Custom Option'},
+      ],
+      storageOptions: {
+        skipBackup: true,
+        path: 'images',
+      },
+    };
+    ImagePicker.showImagePicker(options, response => {
+      console.log('Response = ', response);
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      } else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      } else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+        alert(response.customButton);
+      } else {
+        // You can also display the image using data:
+        // let source = { uri: 'data:image/jpeg;base64,' + response.data };
+        this.setState({
+          file: response,
+        });
+      }
+    });
+  }
+
+  async snedRequest() {
+    const formData = new FormData();
+    console.log(this.state.file.uri);
+    console.log(this.state.file.type);
+    console.log(this.state.file.name);
+    formData.append('photo', {
+      uri: this.state.file.uri,
+      name: this.state.file.name,
+      type: this.state.file.name,
+    });
+    //formData.append('photo', this.state.file)
+    formData.append('Content-Type', this.state.file.type);
+    axios
+      .post('https://hello-ml-4pv5qti4qa-uw.a.run.app/predict', {
+        formData,
+      })
+      .then(res => {
+        console.log(res.data);
+      });
+    /*
+    console.log('fetching');
+    try {
+      let res = await fetch(
+        'https://hello-ml-4pv5qti4qa-uw.a.run.app/predict',
+        {
+          method: 'POST',
+          body: formData,
+        },
+      );
+      let responseJason = await res.json();
+      console.log('Success');
+      console.log(responseJason.text);
+    } catch (err) {
+      console.log('Some thing went wrong...');
+      console.log(JSON.stringify(err));
+      throw err;
+    }
+     */
+  }
+
+  render() {
+    return (
+      <View>
+        <StatusBar barStyle="dark-content" />
+        <SafeAreaView>
+          <ScrollView
+            contentInsetAdjustmentBehavior="automatic"
+            style={styles.scrollView}>
+            <Header />
+            {global.HermesInternal == null ? null : (
+              <View style={styles.engine}>
+                <Text style={styles.footer}>Engine: Hermes</Text>
+              </View>
+            )}
+            <View style={styles.body}>
+              <View style={styles.sectionContainer}>
+                <Text style={styles.sectionTitle}>Step One</Text>
+                <Text style={styles.sectionDescription}>
+                  Edit <Text style={styles.highlight}>App.js</Text> to change
+                  this screen and then come back to see your edits.
+                </Text>
+              </View>
+              <View style={styles.sectionContainer}>
+                <Text style={styles.sectionTitle}>See Your Changes</Text>
+                <Text style={styles.sectionDescription}>
+                  <ReloadInstructions />
+                </Text>
+              </View>
+              <View style={styles.sectionContainer}>
+                <Text style={styles.sectionTitle}>Debug</Text>
+                <Text style={styles.sectionDescription}>
+                  <DebugInstructions />
+                </Text>
+              </View>
+              <View style={styles.sectionContainer}>
+                <Text style={styles.sectionTitle}>Learn More</Text>
+                <Text style={styles.sectionDescription}>
+                  Read the docs to discover what to do next:
+                </Text>
+              </View>
+              <LearnMoreLinks />
+              <View style={styles.sectionContainer}>
+                <Text style={styles.sectionTitle}>API test</Text>
+                <TouchableOpacity>
+                  <Text
+                    styles={styles.sectionDescription}
+                    onPress={this.chooseFile}>
+                    Select image
+                  </Text>
+                </TouchableOpacity>
+                <Image
+                  source={{
+                    uri: this.state.file.uri,
+                  }}
+                  style={styles.img_dimension}
+                />
+                <TouchableOpacity>
+                  <Text
+                    styles={styles.sectionDescription}
+                    onPress={this.snedRequest}>
+                    Send Request
+                  </Text>
+                </TouchableOpacity>
+                <Text>{this.state.label}</Text>
+              </View>
+            </View>
+          </ScrollView>
+        </SafeAreaView>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+  img_dimension: {
+    width: 50,
+    height: 50,
+  },
+});
+
+export default App;
---
+++ README.md
@@ -0,0 +1,59 @@
+#hello_ml_frontend
+
+Frontend for hello_ml project<br>
+It is in React Native and can run on both Android and iOS
+
+Setup
+---
+1. Install Node, Python2, and JDK (run as admin)<br> 
+choco install -y nodejs.install python2 jdk8
+2. Download Android Studio
+3. Install Android SDK<br>
+Welcome Screen->Configure->SDK Manager->Android9(Pie)<br>
+Show Package Details->Android SDK Platform 28 & Intel x86 Atom_64 System Image<br>
+SDK Tools->Show Package Details->Android SDK Build-Tool->28.0.3
+4. Add ANDROID_HOME environment variable<br>
+You can find the actual location of the SDK in the Android Studio "Preferences" dialog, under Appearance & Behavior → System Settings → Android SDK
+5. Add platform-tools to PATH variable<br>
+Usually the platform-tool is C:\Android\tools\bin\platform-tools
+6. Create new virtual device<br>
+Welcome Screen->Configure->AVD Manager
+
+Please visit https://reactnative.dev/docs/environment-setup for more or setup for Mac etc.
+
+Dev Environment
+---
+This is a package created by IntelliJ. The project was then shared on Github by:
+1. VCS->Import into Version Control->Share Project on Github
+
+Cloning this project into your dev environment
+---
+The following step shows how to clone this project in IntelliJ and start contribute to it
+1. Create your own react native project. You can name it whatever but hello_ml_frontend is recommended :)
+2. Verify that your newly created project builds and run, especially if it run on emulator or not!!
+3. Set up git as following<br>
+3.1 VCS->enable version control integration<br> 
+3.2 VCS->Git->Remote->Add the GitHub repository<br>
+3.3 Right click project->Git->repository->branches->Remote->check out, then you will be prompt with an error message, click file there->Delete all file there<br>
+3.4 Then repeat the above
+
+Install Library
+---
+1. Install React Native Image Picker (from the project root)<br>
+npm i react-native-image-picker<br>
+or<br>
+yarn add react-native-image-picker
+2. Linking Dependency<br>
+react-native link react-native-image-picker<br>
+cd android && gradlew clean
+3. Set camera permission for Android
+Project->android->app->src->main->AndroidManifest.xml<br>
+Add the following:
+````
+<uses-permission android:name="android.permission.CAMERA"/>
+<uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE"/>
+````
+4. Install form-data<br>
+npm install --save form-data
+5. Install axios<br>
+npm install axios
